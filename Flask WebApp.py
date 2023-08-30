98066664296a4516f60cfb39cb421ad127655187
Hamza-Suleman <92225185+Hamza-Suleman@users.noreply.github.com>
2023-08-30 09:36:32 +0100

    Run code added

--- Flask WebApp.py
+++ Flask WebApp.py
@@ -3,3 +3,11 @@
 from flask import Flask
 
 EventManager = Flask(__name__)
+
+
+
+
+# Open the app in the browser when this file is executed
+if __name__ == '__main__':
+    webbrowser.open_new('http://127.0.0.1:5000/')  # Open the URL in the default browser
+    EventManager.run()
